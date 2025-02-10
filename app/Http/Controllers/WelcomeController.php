24f0e496956c8af8c8462ad5aaf9b73da96b58ce
dulciobernardo77 <dulciobernardo80@gmail.com>
2025-02-10 23:47:00 +0100

    Update WelcomeController.php , melhorando a aparencia

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -16,32 +16,89 @@ class WelcomeController extends Controller
 
             $featuredCourses = [
             (object)[
-                'image' => '/images/course1.jpg',
+                'image' => '/images/user1.webp',
                 'category' => 'Desenvolvimento Web',
                 'title' => 'Desenvolvimento Full Stack',
                 'description' => 'Aprenda a criar aplicações web completas com as tecnologias mais modernas.',
-                'price' => '997,00'
+                'price' => 'free'
             ],
             (object)[
-                'image' => '/images/course2.jpg',
+                'image' => '/images/user1.webp',
                 'category' => 'Data Science',
                 'title' => 'Ciência de Dados',
                 'description' => 'Domine análise de dados, machine learning e visualização de dados.',
-                'price' => '897,00'
+                'price' => 'free'
             ],
             (object)[
-                'image' => '/images/course3.jpg',
+                'image' => '/images/user1.webp',
                 'category' => 'Mobile',
                 'title' => 'Desenvolvimento Mobile',
                 'description' => 'Crie aplicativos móveis modernos para iOS e Android.',
-                'price' => '797,00'
+                'price' => 'free'
+            ],
+            (object)[
+                'image' => '/images/user1.webp',
+                'category' => 'Desenvolvimento Web',
+                'title' => 'Desenvolvimento Full Stack',
+                'description' => 'Aprenda a criar aplicações web completas com as tecnologias mais modernas.',
+                'price' => 'free'
+            ],
+            (object)[
+                'image' => '/images/user1.webp',
+                'category' => 'JavaScript',
+                'title' => 'Fundamentos de Javascript',
+                'description' => 'Domine O fundamentos do js.aprenda a cria aplicacoes web interativas e modernais',
+                'price' => 'free'
+            ],
+            (object)[
+                'image' => '/images/user1.webp',
+                'category' => 'Mobile',
+                'title' => 'Desenvolvimento Mobile',
+                'description' => 'Crie aplicativos móveis modernos para iOS e Android.',
+                'price' => 'free'
             ]
         ];
 
         $testimonials = [
             [
                 'image' => '/images/user1.jpg',
-                'name' => 'João Silva',
+                'name' => 'Dulcio Bernardo',
+                'role' => 'Desenvolvedor Full Stack',
+                'content' => 'Os cursos da Space seat mudaram minha carreira. Em 6 meses consegui minha primeira vaga como desenvolvedor.'
+            ],
+            [
+                'image' => '/images/user2.jpg',
+                'name' => 'Maria Santos',
+                'role' => 'Data Scientist',
+                'content' => 'A qualidade do conteúdo e o suporte da comunidade são excepcionais. Recomendo fortemente.'
+            ],
+            [
+                'image' => '/images/user3.jpg',
+                'name' => 'Pedro Costa',
+                'role' => 'Mobile Developer',
+                'content' => 'A metodologia prática e os projetos reais fizeram toda a diferença no meu aprendizado.'
+            ],
+            [
+                'image' => '/images/user1.jpg',
+                'name' => 'Dulcio Bernardo',
+                'role' => 'Desenvolvedor Full Stack',
+                'content' => 'Os cursos da Space seat mudaram minha carreira. Em 6 meses consegui minha primeira vaga como desenvolvedor.'
+            ],
+            [
+                'image' => '/images/user2.jpg',
+                'name' => 'Maria Santos',
+                'role' => 'Data Scientist',
+                'content' => 'A qualidade do conteúdo e o suporte da comunidade são excepcionais. Recomendo fortemente.'
+            ],
+            [
+                'image' => '/images/user3.jpg',
+                'name' => 'Pedro Costa',
+                'role' => 'Mobile Developer',
+                'content' => 'A metodologia prática e os projetos reais fizeram toda a diferença no meu aprendizado.'
+            ],
+            [
+                'image' => '/images/user1.jpg',
+                'name' => 'Dulcio Bernardo',
                 'role' => 'Desenvolvedor Full Stack',
                 'content' => 'Os cursos da Space seat mudaram minha carreira. Em 6 meses consegui minha primeira vaga como desenvolvedor.'
             ],
@@ -70,7 +127,7 @@ class WelcomeController extends Controller
             ],
             [
                 'question' => 'Existe suporte durante os cursos?',
-                'answer' => 'Oferecemos suporte completo através de nossa comunidade, fórum exclusivo e mentoria com os instrutores.'
+                'answer' => 'Oferecemos suporte completo através de nossa comunidade no discord, fórum exclusivo e mentoria com os instrutores.'
             ],
             [
                 'question' => 'Posso assistir as aulas offline?',
