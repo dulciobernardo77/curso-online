7ce3f8dcf7562d0095dfc7f702ccb68285d133df
José Jamba <josejamba440@gmail.com>
2025-03-10 15:03:56 +0100

    tb_aluno

---
+++ app/Models/TB_Aluno.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TB_Aluno extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('alunos')->insert([
+            [
+                'nome' => '',
+                'email' => '',
+                'telefone' => '',
+                'curso_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+                ]
+            ]);
+       
+
+       }
+
+       }
